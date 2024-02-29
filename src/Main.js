4e66a0cba087e39cf431bb6233998ec5a94ff577
Gad Nadjar <gadnadjar@gmail.com>
2024-02-29 16:59:12 +0200

    comments

--- src/Main.js
+++ src/Main.js
@@ -72,26 +72,26 @@ function Main() {
           <div className='card2'>
             <img src={stars} alt='stars' className='stars'></img>
             <img src={brad} alt='brad' className='personality'></img>
-            <h2></h2>
-            <h3></h3>
+            <h2>Brad Pitt</h2>
+            <h3>"Really enjoyed the atmosphere."</h3>
           </div>
           <div className='card2'>
             <img src={stars} alt='stars' className='stars'></img>
             <img src={george} alt='george' className='personality'></img>
-            <h2></h2>
-            <h3></h3>
+            <h2>George Clooney</h2>
+            <h3>"The greek salad was excellent!"</h3>
           </div>
           <div className='card2'>
             <img src={stars} alt='stars' className='stars'></img>
             <img src={leonardo} alt='leonardo' className='personality'></img>
-            <h2></h2>
-            <h3></h3>
+            <h2>Leonardo Di Caprio</h2>
+            <h3>"You have to try the Greek Salad!"</h3>
           </div>
           <div className='card2'>
-            <img src={margot} alt='stars' className='stars'></img>
+            <img src={stars} alt='stars' className='stars'></img>
             <img src={margot} alt='brad' className='personality'></img>
-            <h2></h2>
-            <h3></h3>
+            <h2>Margot Robbie</h2>
+            <h3>"Awesome place peaceful atmosphere with delicious food"</h3>
           </div>
         </div>
       </div>
