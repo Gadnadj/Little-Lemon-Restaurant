052df0422f900a0eec1648962c763f7b05e1b50e
Gad Nadjar <gadnadjar@gmail.com>
2024-02-29 17:24:34 +0200

    new changements

--- src/Main.js
+++ src/Main.js
@@ -70,28 +70,39 @@ function Main() {
         <h1>Testimonials</h1>
         <div className='cards2'>
           <div className='card2'>
-            <img src={stars} alt='stars' className='stars'></img>
-            <img src={brad} alt='brad' className='personality'></img>
-            <h2>Brad Pitt</h2>
-            <h3>"Really enjoyed the atmosphere."</h3>
+            <div className='card2-body'>
+              <img src={stars} alt='stars' className='stars'></img>
+              <img src={brad} alt='brad' className='personality'></img>
+              <h2>Brad Pitt</h2>
+              <h3>"Really enjoyed the atmosphere."</h3>
+            </div>
           </div>
+
           <div className='card2'>
-            <img src={stars} alt='stars' className='stars'></img>
-            <img src={george} alt='george' className='personality'></img>
-            <h2>George Clooney</h2>
-            <h3>"The greek salad was excellent!"</h3>
+            <div className='card2-body'>
+              <img src={stars} alt='stars' className='stars'></img>
+              <img src={george} alt='george' className='personality'></img>
+              <h2>George Clooney</h2>
+              <h3>"The greek salad was excellent!"</h3>
+            </div>
           </div>
+
           <div className='card2'>
-            <img src={stars} alt='stars' className='stars'></img>
-            <img src={leonardo} alt='leonardo' className='personality'></img>
-            <h2>Leonardo Di Caprio</h2>
-            <h3>"You have to try the Greek Salad!"</h3>
+            <div className='card2-body'>
+              <img src={stars} alt='stars' className='stars'></img>
+              <img src={leonardo} alt='leonardo' className='personality'></img>
+              <h2>Leonardo Di Caprio</h2>
+              <h3>"You have to try the Greek Salad!"</h3>
+            </div>
           </div>
+
           <div className='card2'>
-            <img src={stars} alt='stars' className='stars'></img>
-            <img src={margot} alt='brad' className='personality'></img>
-            <h2>Margot Robbie</h2>
-            <h3>"Awesome place peaceful atmosphere with delicious food"</h3>
+            <div className='card2-body'>
+              <img src={stars} alt='stars' className='stars'></img>
+              <img src={margot} alt='brad' className='personality'></img>
+              <h2>Margot Robbie</h2>
+              <h3>"Awesome place peaceful atmosphere with delicious food"</h3>
+            </div>
           </div>
         </div>
       </div>
