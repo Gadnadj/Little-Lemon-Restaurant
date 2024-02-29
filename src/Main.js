a17f2758704d287f69f0e9a6c4b6a65f09ede601
Gad Nadjar <gadnadjar@gmail.com>
2024-02-29 15:24:18 +0200

    add cards

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,7 @@
 import restaurant from './images/restauranfood.jpg';
 import greekSalad from './images/greekSalad.jpg';
+import lemon from './images/lemon.jpg';
+import bruschetta from './images/bruschetta.jpg';
 function Main() {
   return (
     <>
@@ -31,24 +33,26 @@ function Main() {
         </div>
 
         <div className='card'>
-          <img src={greekSalad} alt='greek'></img>
+          <img src={bruschetta} alt='greek'></img>
           <div className='card-body'>
             <div className='card-title'>Greek Salad</div>
             <div className='card-text'>
-              Freshly chopped tomatoes, cucumbers, onions, and peppers, topped
-              with feta cheese and Kalamata olives.
+              Our Bruschetta is made from homemade grilled bread that has been
+              smeared with garlic and seasoned with salt and olive oil. Topped
+              with fresh vegetables.
             </div>
             <button>Order Delivery</button>
           </div>
         </div>
 
         <div className='card'>
-          <img src={greekSalad} alt='greek'></img>
+          <img src={lemon} alt='greek'></img>
           <div className='card-body'>
             <div className='card-title'>Greek Salad</div>
             <div className='card-text'>
-              Freshly chopped tomatoes, cucumbers, onions, and peppers, topped
-              with feta cheese and Kalamata olives.
+              This comes straight from grandma’s recipe book, every last
+              ingredient has been sourced and is as authentic as can be
+              imagined.
             </div>
             <button>Order Delivery</button>
           </div>
